b60156ee75e510c7d5364ec775477ad423a77c1a
Th3R3ality <63677818+Th3R3ality@users.noreply.github.com>
2023-12-05 14:03:02 +0100

    day 3 - part 2

--- src/Day3.cpp
+++ src/Day3.cpp
@@ -4,11 +4,16 @@
 
 constexpr int LINE_MAX_LENGTH = 256;
 
-int part1(std::vector<std::string> input, std::string symbols);
+int part1(std::vector<std::string> input);
+int part2(std::vector<std::string> input);
 bool checkForSymbols(std::vector<std::string> input, std::string symbols, int x, int y);
 void createSymbolString(std::vector<std::string>input, std::string& symbolString);
 bool isSymbol(char c, std::string symbols);
-
+bool isNum(char c);
+int getGearRatio(std::vector<std::string> input, int x, int y);
+void getNextNumberPos(std::vector<std::string> input, int x, int y, int& index);
+int getFullNumber(std::vector<std::string> input, int x, int y, int pos);
+bool eol(std::string line, int idx);
 
 void Day3() {
 	std::cout << "DAY 3 v_v" << std::endl;
@@ -22,19 +27,24 @@ void Day3() {
 	while (!inputFile.eof()) {
 		memset(line, 0, LINE_MAX_LENGTH);
 		inputFile.getline(line, LINE_MAX_LENGTH);
-		std::cout << line << std::endl;
+		//std::cout << line << std::endl;
 		input.push_back(line);
 	}
 	
-	createSymbolString(input, symbols);
 
-	int answer1 = part1(input, symbols);
+	//int answer1 = part1(input);
+	int answer2 = part2(input);
 
-	std::cout << "Answer for part 1: " << answer1 << std::endl;
+	//std::cout << "Answer for part 1: " << answer1 << std::endl;
+	std::cout << "Answer for part 2: " << answer2 << std::endl;
 }
 
-int part1(std::vector<std::string> input, std::string symbols) {
-	int answer = 0;
+int part1(std::vector<std::string> input) {
+
+	std::string symbols;
+	createSymbolString(input, symbols);
+
+	int answer = 0;	
 
 	for (int y = 0; y < input.size(); y++) {
 		bool isPart = false;
@@ -75,19 +85,56 @@ int part1(std::vector<std::string> input, std::string symbols) {
 	return answer;
 }
 
+int part2(std::vector<std::string> input) {
+
+	//input = {
+	//	"5*.......",
+	//	"..5...*10",
+	//	".......10"
+	//};
+
+	int answer = 0;
+	for (int y = 0; y < input.size(); y++) {
+		bool isPart = false;
+		int currentNum = 0;
+
+		std::cout << std::endl;
+		if (y != 0)
+			std::cout << input[y - 1] << std::endl;
+		std::cout << input[y] << " <- " << std::endl;
+		if (y != input.size() - 1)
+			std::cout << input[y + 1] << std::endl;
+
+		for (int x = 0; x < input[y].length(); x++) {
+			char c = input[y][x];
+			
+			if (c == '*') {
+				std::cout << "[+] Found gear: " << x << "," << y << std::endl;
+				answer += getGearRatio(input, x, y);
+			}
+		}
+	}
+
+	return answer;
+}
+
 bool checkForSymbols(std::vector<std::string> input, std::string symbols, int x, int y) {
 	int xBegin = (x - 1 < 0 ? 1 : 0);
 	int yBegin = (y - 1 < 0 ? 1 : 0);
 	int yEnd = (y + 1 > input.size()-1 ? 1 : 0);
 	
+	bool hasSymbol = false;
+
 	for (int yOffset = -1 + yBegin; yOffset <= 1 - yEnd; yOffset++) {
 		for (int xOffset = -1 + xBegin; xOffset <= 1; xOffset++) {
 			char c = input[y + yOffset][x + xOffset];
 			if (isSymbol(c, symbols)) {
-				return true;
+				hasSymbol = true;
 			}
 		}
 	}
+	if (hasSymbol)
+		return true;
 
 	return false;
 }
@@ -105,9 +152,100 @@ void createSymbolString(std::vector<std::string> input, std::string& symbolStrin
 	for (int i = 0; i < input.size(); i++) {
 		for (int j = 0; j < input[i].length(); j++) {
 			char c = input[i][j];
-			if (c != '.' && !(c >= '0' && c <= '9')) {
+			if (c != '.' && !isNum(c)) {
 				symbolString.push_back(c);
 			}
 		}
 	}
+}
+
+bool isNum(char c) {
+	return (c >= '0' && c <= '9');
+}
+
+int getGearRatio(std::vector<std::string> input, int x, int y) {
+	std::string symbols = "0123456789";
+	if (!checkForSymbols(input, symbols, x, y)) {
+		std::cout << "[-] Skipping, ratios found\n";
+		return 0;
+	}
+	int index = 0, totalRatio = 1, ratioCount = 0;
+	while (index < 9) {
+		getNextNumberPos(input, x, y, index);
+		if (index < 9) {
+			if (index == -1) {
+				index = 9;
+			}
+			std::cout << "[*] Found gear ratio at offset: " << (int)((float)(index - 1) / 3) - 1 << "," << ((index-1) % 3) - 1 << std::endl;
+			ratioCount++;
+			int ratio = getFullNumber(input, x, y, index - 1);
+			std::cout << "[#] Gear ratio: " << ratio << std::endl;
+			totalRatio = totalRatio * ratio;
+		}
+	}
+	if (ratioCount == 2) {
+		std::cout << "[#*#] Total gear ratio: " << totalRatio << std::endl;
+		return totalRatio;
+	}
+	std::cout << "[!*] Incorrect number of ratios: " << ratioCount << std::endl;
+	return 0;
+}
+
+void getNextNumberPos(std::vector<std::string> input, int x, int y, int& index) {
+	bool yBegin = (y - 1 < 0);
+	if (yBegin && (int)((float)index / 3) == 0) {
+		index += 3;
+	}
+	bool foundNum = false;
+	while (index < 9) {
+		bool xBegin = (x - 1 < 0);
+		if (xBegin && (index % 3 == 0)) {
+			index += 1;
+		}
+
+		bool yEnd = (y + 1 > input.size() - 1);
+		if (yEnd && y + (int)((float)index / 3) - 1 > input.size() - 1) {
+			index = 9;
+			return;
+		}
+
+		char c = input[y + (int)((float)index / 3) - 1][x + (index % 3) - 1];
+
+		if (isNum(c)) {
+			foundNum = true;
+		}
+		if (foundNum && (!isNum(c))) {
+			return;
+		}
+		index++;
+		if (foundNum && (index % 3 == 0)) {
+			if (index == 9)
+				index = -1;
+			return;
+		}
+	}
+	return;
+}
+
+int getFullNumber(std::vector<std::string> input, int x, int y, int pos) {
+	x = x + ((pos % 3) - 1);
+	y = y + ((int)((float)pos / 3) - 1);
+
+	int fullNumber = 0;
+
+	while (x >= 0 && isNum(input[y][x])) {
+		x--;
+	}
+	x++;
+	
+	while (!eol(input[y], x) && isNum(input[y][x])) {
+		fullNumber = (fullNumber * 10) + (input[y][x] - '0');
+		x++;
+	}
+
+	return fullNumber;
+}
+
+bool eol(std::string line, int idx) {
+	return (idx > line.size() - 1);
 }
